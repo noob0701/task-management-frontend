e4a083f4c1fed9094948c5a0fa49dbc8d4102aaf
noob0701 <noob0701@users.noreply.github.com>
2021-08-23 06:38:12 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.fc39a43b0241629943cfabbac4946179.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf4ffb630142b466612d1d095548c38e",
+    "url": "https://noob0701.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "7caee8b147dd410af8b2",
+    "url": "https://noob0701.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "0c489ebf916ee112df53",
+    "url": "https://noob0701.github.io/task-management-frontend/static/js/2.faf65f48.chunk.js"
+  },
+  {
+    "revision": "7caee8b147dd410af8b2",
+    "url": "https://noob0701.github.io/task-management-frontend/static/js/main.5754e49d.chunk.js"
+  },
+  {
+    "revision": "f3e29ea6fe3074271123",
+    "url": "https://noob0701.github.io/task-management-frontend/static/js/runtime~main.05415df9.js"
+  }
+]);
